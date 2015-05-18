71ffad3fe0fedcd16a19641cea500de57a83ff9e
El Bruno <bcapuano@gmail.com>
2015-05-18 12:45:14 +0200

    #working fine in W10

--- SystemInfoV02/SystemInfoV02/MainPage.xaml.cs
+++ SystemInfoV02/SystemInfoV02/MainPage.xaml.cs
@@ -1,30 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SystemInfoV02
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void ButtonGetSystemInformation_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlockSystemInformation.Text = GetSystemInformation();
+        }
+
+        private string GetSystemInformation()
+        {
+            var ret = 
+                @"Processor Count : " + Environment.ProcessorCount + Environment.NewLine +
+                "TickCount:" + Environment.TickCount;
+            return ret;
         }
     }
 }
