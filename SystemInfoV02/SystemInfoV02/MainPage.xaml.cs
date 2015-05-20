f08a894a2afe95e7a0567b05e4a3e1fcd9ac4a1f
El Bruno <bcapuano@gmail.com>
2015-05-20 12:28:36 +0200

    #add routine to get local processor architecture

--- SystemInfoV02/SystemInfoV02/MainPage.xaml.cs
+++ SystemInfoV02/SystemInfoV02/MainPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.ServiceModel.Dispatcher;
-using Windows.Devices.Gpio;
-using Windows.Devices.Spi;
-using Windows.Storage.Streams;
-using Windows.System;
-using Windows.System.Profile;
+using Windows.ApplicationModel;
+using Windows.Devices.Enumeration;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -16,15 +12,43 @@ namespace SystemInfoV02
         {
             InitializeComponent();
         }
+
         private void ButtonGetSystemInformation_Click(object sender, RoutedEventArgs e)
         {
             TextBlockSystemInformation.Text = GetSystemInformation();
+
         }
+
         private string GetSystemInformation()
         {
             var ret =
                 @"Processor Count : " + Environment.ProcessorCount + Environment.NewLine +
                 "TickCount:" + Environment.TickCount + Environment.NewLine;
+
+            var package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            ret += String.Format(
+                               "Name: \"{0}\"\n" +
+                               "Version: {1}.{2}.{3}.{4}\n" +
+                               "Architecture: {5}\n" +
+                               "ResourceId: \"{6}\"\n" +
+                               "Publisher: \"{7}\"\n" +
+                               "PublisherId: \"{8}\"\n" +
+                               "FullName: \"{9}\"\n" +
+                               "FamilyName: \"{10}\"\n" +
+                               "IsFramework: {11}",
+                               packageId.Name,
+                               version.Major, version.Minor, version.Build, version.Revision,
+                               packageId.Architecture,
+                               packageId.ResourceId,
+                               packageId.Publisher,
+                               packageId.PublisherId,
+                               packageId.FullName,
+                               packageId.FamilyName,
+                               package.IsFramework);
+
             return ret;
         }
     }
