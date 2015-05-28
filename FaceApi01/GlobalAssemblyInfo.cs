63a0d01c5ce3b0bff070d8e1c34977614874cfbf
El Bruno <bcapuano@gmail.com>
2015-05-28 11:11:36 +0200

    +add globals assembly information

---
+++ FaceApi01/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(@"Avanade - Showcase")]
+[assembly: AssemblyProduct(@"Avanade - Oltiva Shoppable Window")]
+[assembly: AssemblyCopyright(@"Copyright © Avanade 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion(@"2.0.50527.28")]
+[assembly: AssemblyFileVersion(@"2.0.50527.28")]
