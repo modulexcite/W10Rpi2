c0dc2f029bcd1c648159b52d23bff348bf61e084
El Bruno <bcapuano@gmail.com>
2015-05-28 13:14:01 +0200

    +load images from listview

--- FaceApi01/FaceApi01/MainPage.xaml.cs
+++ FaceApi01/FaceApi01/MainPage.xaml.cs
@@ -1,5 +1,13 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using FaceApi01.Model;
+using Oltiva.Azure.FaceApi;
+
 namespace FaceApi01
 {
     public sealed partial class MainPage : Page
@@ -7,11 +15,40 @@ namespace FaceApi01
         public MainPage()
         {
             InitializeComponent();
+            DataContext = this;
+            InitImageList();
         }
 
-        private void ButtonGetFacesImage1_Click(object sender, RoutedEventArgs e)
+        private async void ButtonGetFacesImage1_Click(object sender, RoutedEventArgs e)
         {
+            string subscriptionKey = "4c138b4d82b947beb2e2926c92d1e514";
+            string fileUrl =
+                @"https://onedrive.live.com/embed?cid=BEF06DFFDB192125&resid=bef06dffdb192125%21627193&authkey=AAo7bmGq3kK1hgg";
+            var client = new FaceServiceClient(subscriptionKey);
+            var faces = await client.DetectAsync(fileUrl, false, true, true);
+        }
 
+        void InitImageList()
+        {
+            var imageList = new List<OneDriveImage>
+            {
+                new OneDriveImage(@"drawing", @"https://brunocapuano.files.wordpress.com/2015/05/20-face-drawings.jpg"),
+                new OneDriveImage(@"family", @"https://brunocapuano.files.wordpress.com/2015/05/family.jpg"),
+                new OneDriveImage(@"princesas", @"https://brunocapuano.files.wordpress.com/2015/05/princesas.jpg"),
+                new OneDriveImage(@"jesus", @"https://brunocapuano.files.wordpress.com/2015/05/jesus-thumbs-up-pic.jpg"),
+                new OneDriveImage(@"Yoda", @"https://brunocapuano.files.wordpress.com/2015/05/yoda-jedi-council.jpg"),
+                new OneDriveImage(@"Star Wars",@"https://brunocapuano.files.wordpress.com/2015/05/star-wars-original.jpg"),
+                new OneDriveImage(@"Star Wars 2015",@"https://brunocapuano.files.wordpress.com/2015/05/star-wars-han-solo-y-chewbacca-2015.jpg")
+            };
+
+            ListViewImages.ItemsSource = imageList;
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var odi = ListViewImages.SelectedItem as OneDriveImage;
+            if (odi == null) return;
+            ImageFaces.Source = new BitmapImage(new Uri(odi.OneDriveUrl, UriKind.RelativeOrAbsolute));
         }
     }
 }
---
+++ FaceApi01/FaceApi01/Model/OneDriveImage.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FaceApi01.Annotations;
+
+namespace FaceApi01.Model
+{
+    public class OneDriveImage : INotifyPropertyChanged
+    {
+        private string _desc;
+        private string _oneDriveUrl;
+
+        public string Desc
+        {
+            get { return _desc; }
+            set
+            {
+                if (value == _desc) return;
+                _desc = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OneDriveUrl
+        {
+            get { return _oneDriveUrl; }
+            set
+            {
+                if (value == _oneDriveUrl) return;
+                _oneDriveUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public OneDriveImage(string desc, string url)
+        {
+            _desc = desc;
+            _oneDriveUrl = url;
+        }
+
+        public override string ToString()
+        {
+            return Desc;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
